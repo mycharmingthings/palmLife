aef176aaac69d4afa363525f9c36d001a84791cc
Yang YouMiao <595010181@qq.com>
2018-03-15 09:18:08 +0800

    3.14

--- express_palmlife/javascripts/index.js
+++ express_palmlife/javascripts/index.js
@@ -17,5 +17,33 @@
 
 })(jQuery);
 
+ 
+// 李航轮播图手机代码 ，-
+// $('.sliderList').append('<li>');
+// $('.sliderList li').last().html($('.sliderList li').first().html());
+// var length = $('.sliderList').children().length;
+// var iIndex = 0;
+// function RightMove(){
+//     iIndex++;
+//     if(iIndex >= length){
+//         iIndex = 1;
+//         $('.sliderList').css('left',0)
+//     }
+//     var left = -iIndex * $(window).width();
+//     $('.sliderList').stop(true).animate({'left':left},500);
+//     if(iIndex > length -2){
+//         $('.point span').removeClass('sliderActive').eq(0).addClass('sliderActive');
+//     }else{
+//         $('.point span').removeClass('sliderActive').eq(iIndex).addClass('sliderActive');
+//     }
+// }
+// function autoMove(){
+//     iTimer = setInterval(function(){
+//         RightMove();
+//     },2000)
+// }
+// autoMove();
+
+
 
 
